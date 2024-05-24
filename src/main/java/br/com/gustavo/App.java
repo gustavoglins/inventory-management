0b12bf19e883ab2ab7dcc31d92cf7236a1f9a0e5
gustavo g lins <gustavoguimaraes.241025@gmail.com>
2024-05-24 00:14:17 -0300

    Creating app.class

---
+++ src/main/java/br/com/gustavo/App.java
@@ -0,0 +1,13 @@
+package br.com.gustavo;
+
+import java.util.Scanner;
+
+public class App {
+
+    private static final Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+
+    }
+}
